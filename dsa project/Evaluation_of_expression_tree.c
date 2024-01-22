03c5de343b06f9de195b3aa3ed5637a122bd3cdb
BIKRAMJIT SARKAR <115685358+biku-tubai@users.noreply.github.com>
2024-01-22 23:43:07 +0530

    Add files via upload

---
+++ dsa project/Evaluation_of_expression_tree.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of the expression tree node
+typedef struct TreeNode {
+    char data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+}tree;
+
+// Function to create a new node(argument:char  return:tree*)
+
+
+// Function to evaluate the expression tree(argument:tree*  return:int)
+
+// Function to check if a character is an operator(argument:char  returns:int)
+
+// Function to insert an equation into an expression tree(argument:char* returns: tree*)
+
+//main function
+int main()
+{
+    
+    return 0;
+}
