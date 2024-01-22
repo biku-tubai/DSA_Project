4bb85deb3939bb83c4d27572f420b37541ed132a
zebessss <155009303+zebessss@users.noreply.github.com>
2024-01-23 01:17:30 +0530

    Update Evaluation_of_expression_tree.c
    added a function to create a new node

--- dsa project/Evaluation_of_expression_tree.c
+++ dsa project/Evaluation_of_expression_tree.c
@@ -9,8 +9,14 @@ typedef struct TreeNode {
 }tree;
 
 // Function to create a new node(argument:char  return:tree*)
-
-
+tree* createNode (char a)
+{ tree* newNode = (tree*) malloc (sizeof(tree));
+newNode -> data = a ;
+newNode->left=NULL ;
+newNode->right=NULL ;
+return newNode ;
+}
+// function to print the infix expression from the expression tree(input : tree*)
 // Function to evaluate the expression tree(argument:tree*  return:int)
 
 // Function to check if a character is an operator(argument:char  returns:int)
